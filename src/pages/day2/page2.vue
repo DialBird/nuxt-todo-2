560a619fe5014133fba944a981b6ed364e432387
Dialbird <rememberthatk@icloud.com>
2021-04-29 20:30:57 +0900

    [master] add day2/pagd2

---
+++ src/pages/day2/page2.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="bg-gray-50">
+    <div class="container">
+      <div class="flex flex-wrap -m-4">
+        <div class="md:w-1/3 p-4">
+          <div class="w-full p-4 border-2 h-auto bg-gray-200">
+            <h2 class="mt-12 mb-1 font-mono text-xs">CATEGORY</h2>
+            <h1 class="mb-4 font-mono font-bold">
+              Raclette Blueberry Nextious Level
+            </h1>
+            <p class="mb-4">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              class="block mb-20 text-purple-500"
+              >Learn more</a
+            >
+            <span class="pr-2 text-xs border-r-2 border-gray-300">1.2K</span>
+            <span class="text-xs">Talk 6</span>
+          </div>
+        </div>
+        <div class="md:w-1/3 p-4">
+          <div class="w-full p-4 border-2 h-auto bg-gray-200">
+            <h2 class="mt-12 mb-1 font-mono text-xs">CATEGORY</h2>
+            <h1 class="mb-4 font-mono font-bold">
+              Raclette Blueberry Nextious Level
+            </h1>
+            <p class="mb-4">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              class="block mb-20 text-purple-500"
+              >Learn more</a
+            >
+            <span class="pr-2 text-xs border-r-2 border-gray-300">1.2K</span>
+            <span class="text-xs">Talk 6</span>
+          </div>
+        </div>
+        <div class="md:w-1/3 p-4">
+          <div class="w-full p-4 border-2 h-auto bg-gray-200">
+            <h2 class="mt-12 mb-1 font-mono text-xs">CATEGORY</h2>
+            <h1 class="mb-4 font-mono font-bold">
+              Raclette Blueberry Nextious Level
+            </h1>
+            <p class="mb-4">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              class="block mb-20 text-purple-500"
+              >Learn more</a
+            >
+            <span class="pr-2 text-xs border-r-2 border-gray-300">1.2K</span>
+            <span class="text-xs">Talk 6</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="">
+</style>
