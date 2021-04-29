a3d36b7c675dd994f3a8b5b1bef6ed0aa93f689f
Dialbird <rememberthatk@icloud.com>
2021-04-29 22:04:51 +0900

    [master] page4

---
+++ src/pages/day2/page4.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="bg-gray-100">
+    <div class="container">
+      <div class="flex flex-wrap">
+        <div class="w-full rounded overflow-hidden bg-white">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/vue-mastery.appspot.com/o/flamelink%2Fmedia%2F1577760969400_connecttech2019.jpg?alt=media&token=d5f6f7d4-06ab-4fdb-9cf3-620f594554e0"
+            class="h-48 object-cover"
+          />
+          <div class="p-4 flex">
+            <div class="text-left">
+              <h2 class="text-xl font-extrabold">Connect.Tech 2019</h2>
+              <p class="mb-2 italic tracking-wider text-gray-500">Japan USA</p>
+              <p class="text-sm">10 Session Talks</p>
+            </div>
+            <div class="ml-auto my-auto">
+              <button
+                class="py-2 px-6 transition bg-white hover:bg-purple-600 text-purple-600 hover:text-white border-2 border-purple-600 rounded-full font-bold tracking-wide focus:outline-none"
+              >
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="">
+</style>
